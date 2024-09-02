7c40901fc8ba1e2113f204af7d3b74f5a14b1273
BhautikKhunt0 <bhautikkhunt7622@gmail.com>
2024-09-02 10:47:17 +0530

    image add assets

--- src/app/pages/digitalmarketing/healthcare/healthcare.component.ts
+++ src/app/pages/digitalmarketing/healthcare/healthcare.component.ts
@@ -7,9 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HealthcareComponent {
   images = {
-    doctorImage:
-      'https://s3-alpha-sig.figma.com/img/4287/6d48/cd1f302610ee5eb0d13fc66fcfa4e303?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LV1ilul-AJoNbuYNzT4-clVyISz0DT-VGowOz6vd2UgNLEE1nr2fjlajyvPPI7zI3UzHpYQ3HWu~itmavZUJxB8Z1VUGOPzYGXxupckBh9ayxB2D87kd~8e2zLU1iiNGjiHbMwmzEvNr-~HYXmJK8frzSLHw2KpWq3prQWsloGlDr761HCf8dNJsdmVyqrU69ynp~CJWNxdp4ztcrAEkbi3Zd2-OLEzHAAxgefiOEQysmSOS~bLwdETjAf0~yg5WkXdw6A9PEWFQGHlD1dHFtejIO-~xisd~QbWFsjO7V6Mk5vtvcxWEXicMAfYdkeS1HK7yI-BNhJGrIpO09p9Wwg__',
-    discussionImage:
-      'https://s3-alpha-sig.figma.com/img/1e81/988b/f04be4fd0fa24de4330db4c5d9441c47?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AXeMO0hgMurR~D1WkhpUuR8pytPo8l3WGHveGsdXbKCWz-RW6PcWDBqg-60LwmwL5XmyqaLWkhmz4YNScKHr0eIhAlp5hYPag2YkwGGOp5GcKVVrhIeabaBmphCJd5A0vRzKRsgTfaFq~A7bd8W~K~F5Ml6QY15NtIlX27cIKb4QUyLaVE-RBXJI9bsNNm~pde5hjOTVjyPv3kEYAuTqZc-z9a1d5DD00OdMAvWL4KhtWZBSpnkb42pgiTqmNokSOJgo49dlANqTEnHmRJKhVvihI8TfLs45M5qu4UMwiCHnDZhhFGqFMaNmk449Gt-RgHxXwjXC8si6WGSbK-XfTA__',
+    doctorImage: '../../../../assets/healthcare/doctor.jpeg',
+    discussionImage: '../../../../assets/healthcare/discussion.jpeg',
   };
 }
